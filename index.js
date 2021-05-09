cec44d7f73be72d7fc18f45d481f893c49ae84fe
nickbalan <nickolae.balan@gmail.com>
2021-05-09 23:25:32 +0200

    This commit has added require Express in the index.js and added an array with the list of Top 10 Worldwide Box Office (2021).

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+	morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+	methodOverride = require('method-override');
+
+// Adds an arry with the list of Top 10 Worldwide Box Office (2021)
+let WorldwideBoxOffice = [
+	{
+		title:'Hi, Mom',
+		director:'Jia Ling'
+	},
+	{
+		title:'Detective Chinatown 3',
+		director:'Chen Sicheng'
+	},
+	{
+		title:'Godzilla vs. Kong',
+		director:'Adam Wingard'
+	},
+	{
+		title:'A Writers Odyssey',
+		director:'Lu Yang'
+	},
+	{
+		title:'Sister',
+		director:'Ruoxin Yin'
+	},
+	{
+		title:'Tom and Jerry',
+		director:'William Hanna'
+	},
+	{
+		title:'Raya and the Last Dragon',
+		director:'Don Hall'
+	},
+	{
+		title:'Endgame',
+		director:'Anthony Russo'
+	},
+	{
+		title:'Boonie Bears: The Wild Life',
+		director:'Leon Ding'
+	},
+	{
+		title:'Evangelion: 3.0+1.0 Thrice Upon a Time',
+		director:'Hideaki Anno'
+	}
+];
+
+//Listens for requests on port 8080
+app.listen(8080, () => {
+	console.log('This app is listening on port 8080.');
+});
