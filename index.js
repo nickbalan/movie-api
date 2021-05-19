5af3f248394d1d34632105abb61f4b758144a16c
nickbalan <nickolae.balan@gmail.com>
2021-05-19 10:24:37 +0200

    This commit has corrected const variable declaration.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 	morgan = require('morgan');
-const app = express();
-const bodyParser = require('body-parser');
+	app = express();
+	bodyParser = require('body-parser');
 	methodOverride = require('method-override');
 	uuid = require('uuid');
 
