cb4fa790d50ba68b547dc5e183f3343d00eb60b3
nickbalan <nickolae.balan@gmail.com>
2021-06-26 03:38:45 +0200

    This commit has added auth.js file and passport.js for implementing the Passport Strategies and the Authentication Logic into project.

---
+++ auth.js
@@ -0,0 +1,38 @@
+const jwtSecret = 'your_jwt_secret';
+
+const jwt = require('jsonwebtoken'),
+	password = require('passport');
+
+require('./passport');
+
+// JWTStrategy
+let generateJWTToken = (user) => {
+	return jwt.sign(user, jwtSecret, {
+		subject: user.Username,
+		expiresIn: '7d', // The token expires in 7 days
+		algorithm: 'HS256' // The algorithm used to 'sign' or encode the values of the JWT
+	});
+}
+
+
+// POST login. LocalStrategy
+module.exports = (router) => {
+	router.post('/login', (req, res) => {
+		passport.authenticate('local', {session: false}, (error, user, info) => {
+			if (error || !user) {
+				return res.status(400).json({
+					message: 'Something is not right',
+					user: user
+				});
+			}
+			req.login(user, {session: false}, (error) => {
+				if (error) {
+					res.send(error);
+				}
+				let token = generateJWTToken(user.toJSON());
+				return res.json({user, token});
+			});
+		})(req, res);
+	});
+}
+
