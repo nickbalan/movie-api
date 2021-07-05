fec313dfde9e633241dd77b8e166a832702add90
nickbalan <nickolae.balan@gmail.com>
2021-07-05 18:15:53 +0200

    This commit has updated the package.json and also the app.listen function from the index file.

--- index.js
+++ index.js
@@ -282,6 +282,7 @@ app.use((err, req, res, next) => {
 });
 
 //Listens for requests on port 8080
-app.listen(8080, () => {
-	console.log('This app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+	console.log('This app is listening on port: ' + port);
 });
