a05877b9d65c5a50463fa575be7325a7f055fc47
nickbalan <nickolae.balan@gmail.com>
2021-07-05 16:06:19 +0200

    This commit has added data validation to the endpoints that create or updates user data.

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const	Models = require('./models.js');
 //Integrating CORS
 const cors =require('cors');
 
-let auth = require('./auth')(app);
-
-//Integrationg Passport module
+//Integrating Passport module
 const passport = require('passport');
+
+//Integrating Express Validator
+const {check, validationResult} = require('express-validator');
 
 const	movie = Models.Movie;
 const	user = Models.User;
@@ -25,11 +26,15 @@ const genre = Models.Genre;
 //Connect Mongoose to the local database
 mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
+//Implements auth
+let auth = require('./auth')(app);
+
 //Implements the Logs with Morgan in Express
 app.use(morgan('common'));
 
 require('./passport');
 
+//Implements CORS
 app.use(cors());
 
 //Implements Error Handling in Express
@@ -121,32 +126,79 @@ app.get('/directors/:Name', passport.authenticate('jwt', {session: false}), (req
 });
 
 // Adds a new user to the list of Users.
-app.post('/users', (req, res) => {
-  user.findOne({Username: req.body.Username})
-		.then((users) => {
-			if (users) {
-				return res.status(400).send(req.body.Username + ' already exists')
-			} else {
-				user.create({
-					Username: req.body.Username,
-					Password: req.body.Password,
-					Email: req.body.Email,
-					Birthday: req.body.Birthday,
-				})
-				.then((users) => {
-					res.status(201).json(users).send('New User created')
-				})
-				.catch((error) => {
-					console.error(error);
-					res.status(500).send('Error: ' + error);	
-				})
-			}
-		})
-		.catch((error) => {
-			console.error(error);
-			res.status(500).send('Error: ' + error);	
-		});
-});
+app.post('/users', [
+		check('Username', 'Username is required').isLength({min: 6}),  //Only a minimum value of 6 characters are allowed
+		check('Username', 'Username contains non alphanumeric characters - not allowed.'),  //Only alphanumeric characters are allowed
+		check('Password', 'Password is required').not().isEmpty(),  //Which means "opposite of isEmpty". The password is not allowed to be "empty"
+		check('Email', 'Email does not appear to be valid.').isEmail() //Only email format is allowed
+	], (req, res) => {
+
+		//Checks the validation object for errors
+		let error = validationResult(req);
+		if (!error.isEmpty()) {
+			return res.status(422).json({error: error.array()});
+		}
+		
+		let hashedPassword = user.hashPassword(req.body.Password); //Hashes the password
+		user.findOne({Username: req.body.Username}) // Search to see if a user already exists
+			.then((users) => {
+				if (users) {
+					return res.status(400).send(req.body.Username + ' already exists')
+				} else {
+					user.create({
+						Username: req.body.Username,
+						Password: hashedPassword,
+						Email: req.body.Email,
+						Birthday: req.body.Birthday,
+					})
+					.then((users) => {
+						res.status(201).json(users).send('New User created')
+					})
+					.catch((error) => {
+						console.error(error);
+						res.status(500).send('Error: ' + error);	
+					})
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				res.status(500).send('Error: ' + error);	
+			});
+	});
+
+// Updates username from the list of Users.
+app.put('/users/:Username', passport.authenticate('jwt', {session: false}), [
+		check('Username', 'Username is required').isLength({min: 6}),  //Only a minimum value of 6 characters are allowed
+		check('Username', 'Username contains non alphanumeric characters - not allowed.'),  //Only alphanumeric characters are allowed
+		check('Password', 'Password is required').not().isEmpty(),  //Which means "opposite of isEmpty". The password is not allowed to be "empty"
+		check('Email', 'Email does not appear to be valid').isEmail() //Only email format is allowed
+	], (req, res) => {
+
+		//Checks the validation object for errors
+		let error = validationResult(req);
+		if (!error.isEmpty()) {
+			return res.status(422).json({error: error.array()});
+		}
+		
+		let hashedPassword = user.hashPassword(req.body.Password); //Hashes the password	
+	  updateUser = user.findOneAndUpdate({Username: req.params.Username}, {
+	  	$set:{
+	  		Username: req.body.Username,
+	  		Password: hashedPassword,
+	  		Email: req.body.Email,
+	  		Birthday: req.body.Birthday
+	  	}
+	  },
+	  {new: true},
+	  (err, updateUser) => {
+	  	if(err) {
+	  		console.error(err);
+	  		res.status(500).send('Error: ' + err);		
+	  	} else {
+	  		res.json(updateUser);
+	  	}
+	  });  
+	});
 
 // Gets the data about all users.
 app.get('/users/all', passport.authenticate('jwt', {session: false}), (req, res) => {
@@ -170,27 +222,6 @@ app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req
 			console.error(error);
 			res.status(500).send('Error: ' + error);
 		});
-});
-
-// Updates username from the list of Users.
-app.put('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) => {
-  updateUser = user.findOneAndUpdate({Username: req.params.Username}, {
-  	$set:{
-  		Username: req.body.Username,
-  		Password: req.body.Password,
-  		Email: req.body.Email,
-  		Birthday: req.body.Birthday
-  	}
-  },
-  {new: true},
-  (err, updateUser) => {
-  	if(err) {
-  		console.error(err);
-  		res.status(500).send('Error: ' + err);		
-  	} else {
-  		res.json(updateUser);
-  	}
-  });  
 });
 
 // Adds the favorite movies to the list of favorites
--- passport.js
+++ passport.js
@@ -19,8 +19,13 @@ passport.use(new LocalStrategy({
 			}
 
 			if (!user) {
-				console.log('incorrect username');
-				return callback(null, false, {message: 'Incorrect username or password.'});
+				console.log('Incorrect username');
+				return callback(null, false, {message: 'Incorrect username.'});
+			}
+
+			if (!user.validatePassword(password)) {
+				console.log('Incorrect password');
+				return callback(null, false, {message: 'Incorrect password.'});
 			}
 
 			console.log('finished');
