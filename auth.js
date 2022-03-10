2e576b83ae452e352b9ed85f82c4d09d8a273e18
nickbalan <nickolae.balan@gmail.com>
2022-03-10 18:23:43 +0100

    Adding JSDoc Documentation

--- auth.js
+++ auth.js
@@ -4,7 +4,12 @@ const passport = require('passport');
 
 require('./passport'); // the local passport file
 
-// JWTStrategy
+/**
+ * @description Generates a JWT for a user and returns the token as a string
+ * @method generateJWTToken
+ * @param {object} user Object containing all of the users data
+ * @returns {string} - JWT for the logged in user
+ */
 let generateJWTToken = (user) => {
 	return jwt.sign(user, jwtSecret, {
 		subject: user.Username,
@@ -13,23 +18,38 @@ let generateJWTToken = (user) => {
 	});
 }
 
-
+/**
+ * @description Endpoint to login the user
+ * @method POST LoginUser
+ * @param {string} endpoint - /login?Username=[Username]&Password=[Password]
+ * @returns {object} - JSON object containing data for the user and a new JWT. 
+ * { user: {  
+ *   _id: <string>,  
+ *   Username: <string>,
+ *   Password: <string> (hashed),  
+ *   Email: <string>,  
+ *   Birthday: <string>,  
+ *   Watchlist: [<string>]  
+ *   },   
+ *   token: <string>   
+ * }
+ */
 // POST login. LocalStrategy
 module.exports = (router) => {
 	router.post('/login', (req, res) => {
-		passport.authenticate('local', {session: false}, (error, user, info) => {
+		passport.authenticate('local', { session: false }, (error, user, info) => {
 			if (error || !user) {
 				return res.status(400).json({
-					message: 'Something is not right',
+					message: 'Something is wrong. Try again.',
 					user: user
 				});
 			}
-			req.login(user, {session: false}, (error) => {
+			req.login(user, { session: false }, (error) => {
 				if (error) {
 					res.send(error);
 				}
 				let token = generateJWTToken(user.toJSON());
-				return res.json({user, token});
+				return res.json({ user, token });
 			});
 		})(req, res);
 	});
--- passport.js
+++ passport.js
@@ -7,32 +7,46 @@ let Users = Models.User;
 let JWTStrategy = passportJWT.Strategy;
 let ExtractJWT = passportJWT.ExtractJwt;
 
+/**
+ * @description Middleware logic for checking login credentials for a user.
+ * Function is called when a user logs in.
+ * First ensures the user exists, then checks that the password is correct.
+ * @method loginStrategy
+ * @returns {boolean} Returns true if credentials are valid, false otherwise
+ */
 passport.use(new LocalStrategy({
 	usernameField: 'Username',
 	passwordField: 'Password'
-	}, (username, password, callback) => {
-		console.log(username + ' ' + password);
-		Users.findOne({Username: username}, (error, user) => {
-			if (error) {
-				console.log(error);
-				return callback(error);
-			}
+}, (username, password, callback) => {
+	console.log(username + ' ' + password);
+	Users.findOne({ Username: username }, (error, user) => {
+		if (error) {
+			console.log(error);
+			return callback(error);
+		}
 
-			if (!user) {
-				console.log('Incorrect username');
-				return callback(null, false, {message: 'Incorrect username.'});
-			}
+		if (!user) {
+			console.log('Incorrect username');
+			return callback(null, false, { message: 'Incorrect username.' });
+		}
 
-			if (!user.validatePassword(password)) {
-				console.log('Incorrect password');
-				return callback(null, false, {message: 'Incorrect password.'});
-			}
+		if (!user.validatePassword(password)) {
+			console.log('Incorrect password');
+			return callback(null, false, { message: 'Incorrect password.' });
+		}
 
-			console.log('finished');
-			return callback(null, user);
-		});
-	}));
+		console.log('finished');
+		return callback(null, user);
+	});
+}));
 
+/**
+* @description Middleware logic for checking JWT of a user. <br>
+* Function is called to check validation before accessing restricted endpoints.<br>
+* Decodes the JWT and then checks to see if the encoded user exists on the server
+* @method jwtStrategy
+* @returns {boolean} Returns true if JWT is valid, false otherwise
+*/
 passport.use(new JWTStrategy({
 	jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
 	secretOrKey: 'your_jwt_secret'
